dbe84abc1c83ab25eedd9b4b98e3081b7d1fa89d
Vincenzo Lucente <vincenzo.lucente@finconsgroup.local>
2022-07-04 09:00:17 +0200

    implemented auth provider

--- src/main/java/guru/springframework/sfgrestbrewery/security/WebSecurityConfiguration.java
+++ src/main/java/guru/springframework/sfgrestbrewery/security/WebSecurityConfiguration.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import guru.springframework.sfgrestbrewery.security.auth.CustomAuthProviderInterface;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +21,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
-	@Bean
+	@Autowired
+	private CustomAuthProviderInterface authProvider;
+	
+	/*@Bean
 	AuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider provider =
 				new DaoAuthenticationProvider();
@@ -26,8 +32,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 		provider.setUserDetailsService(userDetailsService);
 		provider.setPasswordEncoder(new BCryptPasswordEncoder());
 		return provider;
-	}
+	}*/
 	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.authenticationProvider(authProvider);
+	}
+		
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
 			.antMatchers("/api/v1/beers").hasAnyAuthority("ADMIN", "USER")
