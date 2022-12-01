9c9019a5a2bea4404491f9085361e84de6028d3e
Vincenzo Lucente <vincenzo.lucente@finconsgroup.local>
2022-12-01 15:51:12 +0100

    last changes

--- src/main/java/guru/springframework/sfgrestbrewery/security/MultiUserProvider.java
+++ src/main/java/guru/springframework/sfgrestbrewery/security/MultiUserProvider.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgrestbrewery.security;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -12,13 +15,33 @@ import org.springframework.stereotype.Component;
 @Profile("multiple")
 public class MultiUserProvider extends FinconsAuthProvider {
 
+    private Map<String,String> fakeDatabase;
+
+    MultiUserProvider(){
+        fakeDatabase = new HashMap<>();
+        for(int i = 1; i <= 5; i++){
+            fakeDatabase.put("user"+i,"password"+i);
+        }
+    }
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return new UsernamePasswordAuthenticationToken("pippo","pippo", Collections.emptyList());
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        if (!isValidUser(username, password)){
+            throw new BadCredentialsException("You have provided bad credentials!!!");
+        }
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username,password, Collections.emptyList());
+        System.out.println("Your token is: "+token.getName());
+        return token;
     }
 
     @Override
     public boolean isValidUser(String username, String password) {
-        return false;
+        return fakeDatabase != null
+                && username != null
+                && password != null
+                && fakeDatabase.containsKey(username)
+                && fakeDatabase.get(username).equals(password);
     }
 }
